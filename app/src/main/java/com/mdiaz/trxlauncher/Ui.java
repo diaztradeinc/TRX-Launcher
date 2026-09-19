829964ac776a805adaaaca6a3a0215af806493f5
diaztradeinc <79354866+diaztradeinc@users.noreply.github.com>
2026-09-19 08:07:55 -0400

    Fix clean UI helper import

--- app/src/main/java/com/mdiaz/trxlauncher/Ui.java
+++ app/src/main/java/com/mdiaz/trxlauncher/Ui.java
@@ -6,6 +6,7 @@ import android.graphics.Typeface;
 import android.graphics.drawable.GradientDrawable;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.*;
 
 final class Ui {
